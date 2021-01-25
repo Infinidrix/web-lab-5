65779c9214a7f0c0904b232d8b17bd690b2d979b
Infinidrix <biruksolomon11@gmail.com>
2021-01-25 11:01:05 +0300

    add additional functions

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -1,4 +1,4 @@
-let options = ["", " + ", " - ", " * ", " / ", " = ", " ** "];
+let options = ["", " + ", " - ", " * ", " / ", " = ", " ** ", " % ", " ^ ", "<<"];
 // define UI variables
 
 let calc = document.querySelector("#Calculator");
