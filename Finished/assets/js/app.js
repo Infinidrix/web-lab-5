cbcb3ed2055baa897db13ae463d33fe7154044f4
Infinidrix <biruksolomon11@gmail.com>
2021-01-28 09:11:17 +0300

    add edit task support

--- Finished/assets/js/app.js
+++ Finished/assets/js/app.js
@@ -21,7 +21,7 @@ clearBtn.addEventListener('click', clearAllTasks);
 //   Filter Task 
 filter.addEventListener('keyup', filterTasks);
 // Remove task event [event delegation]
-taskList.addEventListener('click', removeTask);
+taskList.addEventListener('click', modifyTask);
 // Event Listener for reload 
 reloadIcon.addEventListener('click', reloadPage);
 document.addEventListener('DOMContentLoaded', function() {
@@ -32,9 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
 desendingBtn.addEventListener('click', (e) => sort(true));
 ascendingBtn.addEventListener('click', (e) => sort(false));
 
-
-function sort(isDecending) {
-    if ((isAscending ^ isDecending)) return
+function sort(makeDesending) {
+    if ((isAscending ^ makeDesending)) return
 
     let tasks = taskList.children.length;
     for (let i = 1; i < tasks; i++) {
@@ -65,10 +64,16 @@ function addNewTask(e) {
     // Create text node and append it 
     li.appendChild(document.createTextNode(taskInput.value));
     // Create new element for the link 
-    const link = document.createElement('a');
+    let link = document.createElement('a');
     // Add class and the x marker for a 
     link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fa fa-remove" aria-hidden="true"></i>';
+    // Append link to li
+    li.appendChild(link);
+    link = document.createElement('a');
+    // Add class and the x marker for a 
+    link.className = 'edit-item secondary-content';
+    link.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
     // Append link to li
     li.appendChild(link);
     // Append to UL 
@@ -114,12 +119,14 @@ function filterTasks(e) {
 }
 
 // Remove Task function definition 
-function removeTask(e) {
+function modifyTask(e) {
+    console.log(e);
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('Are You Sure about that ?')) {
             e.target.parentElement.parentElement.remove();
-
         }
+    } else if (e.target.parentElement.classList.contains('edit-item')){
+        e.target.parentElement.previousSibling.previousSibling.data = prompt("Edit task name");
     }
 }
 
