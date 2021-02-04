a2d5a3be613d7e0ba4d0a99e83d0eca3ed986e34
Infinidrix <biruksolomon11@gmail.com>
2021-02-04 08:42:33 +0300

    replace calculation method

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -1,8 +1,32 @@
-let options = ["", " + ", " - ", " * ", " / ", " = ", " ** ", " % ", " ^ ", "<<"];
+let options = ["", " + ", " - ", " * ", " / ", " = ", " ^ ", " % ", ".", "^ 2"];
+let opsStack = []
+let numStack = [];
 // define UI variables
 
 let calc = document.querySelector("#Calculator");
 let output = document.querySelector("#output");
+
+function add(num1, num2){
+    return num1 + num2;
+}
+function subs(num1, num2){
+    return num1 - num2;
+}
+function mult(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    if (num2 === 0){
+        throw EvalError("Can't have 0 as divisor");
+    }
+    return num1 / num2;
+}
+function pow(num1, num2){
+    return num1 ** num2
+}
+function mod(num1, num2){
+    return num1 % num2;
+}
 
 calc.addEventListener('click', (e) => {
     let src = e.target.id;
@@ -19,10 +43,32 @@ calc.addEventListener('click', (e) => {
     }
 })
 
-function calculateExpr(value){
+function calculateExpr(expr){
     try{
-        return Function(`return ${value}`)();
+        if (expr.includes("+")){
+            var loc = expr.indexOf("+"); 
+            return add(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));
+        } else if (expr.includes("-")){
+            var loc = expr.indexOf("-"); 
+            return subs(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));
+        } else if (expr.includes("*")){
+            var loc = expr.indexOf("*"); 
+            return mult(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));
+        } else if (expr.includes("/")){
+            var loc = expr.indexOf("/"); 
+            return div(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));
+        } else if (expr.includes("^")){
+            var loc = expr.indexOf("^");
+            return pow(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));
+        } else if (expr.includes("%")){
+            var loc = expr.indexOf("%");
+            return mod(calculateExpr(expr.slice(0, loc)), calculateExpr(expr.slice(loc + 1, expr.length)));  
+        } 
+        else {
+            return parseFloat(expr)
+        }
     } catch (e) {
+        console.log(e);
         return "";
     }
 }
